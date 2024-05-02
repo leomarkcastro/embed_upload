0863558be3b11e4fd2301a7d03a71cb0ed001c1d
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-05-02 09:15:39 +0800

    feat: updated-upload-logic

--- src/lib/s3/presigned.ts
+++ src/lib/s3/presigned.ts
@@ -4,7 +4,11 @@ const ClientOpts: ClientOptions = {
   accessKey: process.env.S3_ACCESS_KEY ?? "",
   secretKey: process.env.S3_SECRET_KEY ?? "",
   endPoint: process.env.S3_ENDPOINT ?? "",
+  region: process.env.S3_REGION ?? "",
 };
+
+const BUCKET_NAME = process.env.S3_BUCKET_NAME ?? "";
+const PREFIX = process.env.S3_PATH_PREFIX ?? "";
 
 const client = new Client(ClientOpts);
 
@@ -12,6 +16,10 @@ export const getPresignedUrl = async (
   objectName: string,
   expiry: number = 24 * 60 * 60
 ): Promise<string> => {
-  const url = await client.presignedPutObject("tests", objectName, expiry);
+  const url = await client.presignedPutObject(
+    BUCKET_NAME,
+    PREFIX + objectName,
+    expiry
+  );
   return url;
 };
